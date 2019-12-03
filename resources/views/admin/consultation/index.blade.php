e2052af80ebcd75a2f93fe08814653cea71db0e9
Kevin Romero <kevin.romero@red4g.net>
2019-12-03 07:14:52 -0600

    assets

--- resources/views/admin/consultation/index.blade.php
+++ resources/views/admin/consultation/index.blade.php
@@ -34,40 +34,66 @@ Reload
             <div class="row">
                 <div class="col-md-6">
                     @foreach($tooth as $i => $teeth)
-                        @if($i < 32)
-                            <div class="col-16 text-center">
-                                <label>{{$teeth->name}}</label><br>
-                                <img src="{{asset('assets/' . $teeth->img)}}" alt="{{$teeth->name}}">
-                            </div>
-                            @if((($i+1) % 8) == 0)
-                                </div>
-                                <div class="col-md-6">
-                            @endif
-                        @endif
-                    @endforeach
+                    @if($i < 32) <div class="col-16 text-center">
+                        <label>{{$teeth->name}}</label><br>
+                        <img src="{{asset('assets/' . $teeth->img)}}" alt="{{$teeth->name}}">
                 </div>
+                @if((($i+1) % 8) == 0)
             </div>
-            <br>
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3">
-                    @foreach($tooth as $i => $teeth)
-                        @if($i > 31)
-                            <div class="col-10 text-center">
-                                <label>{{$teeth->name}}</label><br>
-                                <img src="{{asset('assets/' . $teeth->img)}}" alt="{{$teeth->name}}">
-                            </div>
-                        @endif
-                    @endforeach
-                </div>
+            <div class="col-md-6">
+                @endif
+                @endif
+                @endforeach
             </div>
-            <br>
-            <button type="submit" class="btn btn-lg btn-primary">Terminar</button>
         </div>
+        <br>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                @foreach($tooth as $i => $teeth)
+                @if($i > 31)
+                <div class="col-10 text-center">
+                    <label>{{$teeth->name}}</label><br>
+                    <img src="{{asset('assets/' . $teeth->img)}}" alt="{{$teeth->name}}">
+                </div>
+                @endif
+                @endforeach
+            </div>
+        </div>
+        <br>
+        <button type="submit" class="btn btn-lg btn-primary">Terminar</button>
+    </div>
     </div>
 </form>
 @endif
+<button type="button" class="btn btn-primary btn-lg" onclick="clickTeeth()">
+  Launch demo modal
+</button>
+
+<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+            </div>
+            <div class="modal-body">
+                <input type="text" id="myInput">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
 
+@section('scripts')
+    @parent
+    {{Html::script('assets/jquery.js')}}
+    {{Html::script('assets/bootstrap.min.js')}}
+    {{Html::script('assets/teeth.js')}}
+@endsection
 
 @section('styles')
 @parent
